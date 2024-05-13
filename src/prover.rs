0e1b1c946e2d2e32a3b960173a8e83c8561c63f8
OrangeFender <zyfdmap@126.com>
2024-05-14 00:43:25 +0800

    update prover

--- src/prover.rs
+++ src/prover.rs
@@ -1,9 +1,11 @@
 use blstrs::{G1Projective, Scalar};
 use rand::Rng;
+use rand_core::le;
 use crate::public_parameters::PublicParameters;
 use crate::util;
 pub struct Prover {
     bit_vector: Vec<Scalar>, 
+    s_blinding: Vec<Scalar>,
 }
 
 impl Prover {
@@ -12,8 +14,13 @@ impl Prover {
         let length = pp.get_n_b();
         let mut rng = rand::thread_rng();
         let bit_vector = (0..length).map(|_| util::random_bit_scalar(&mut rng)).collect();
+        
+        let s_blinding = (0..length).map(|_| util::random_scalar(&mut rng)).collect();
+        
+        
         Self {
             bit_vector,
+            s_blinding,
         }
 
     }
