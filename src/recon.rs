927b66de020d717e9d42f83e638969bc8e71c361
QiusuoJC <jasonqiusuo6677@gmail.com>
2024-05-18 23:11:50 +0800

    JasonCheng modified 2024.5.18 23:12

--- src/recon.rs
+++ src/recon.rs
@@ -8,31 +8,31 @@ use crate::{evaluation_domain::BatchEvaluationDomain, lagrange::lagrange_coeffic
 // 第7步重构
 // 调用m次来恢复f_1(0),...,f_m(0)，n是Client的个数
 // players 表示所需重构的prover的个数，share_f和share_r对应其拥有的秘密份额
-pub fn reconstruct_com(ct_coms: &Vec<G1Projective>, share_f: &Vec<Scalar>, share_r: &Vec<Scalar>, players: &Vec<usize>, n:usize, commit_base: &CommitBase) -> (Scalar, Scalar) {
-    let batch_dom = BatchEvaluationDomain::new(n);
-    let lagr = lagrange_coefficients_at_zero(&batch_dom, players.as_slice());
+// pub fn reconstruct_com(ct_coms: &Vec<G1Projective>, share_f: &Vec<Scalar>, share_r: &Vec<Scalar>, players: &Vec<usize>, n:usize, commit_base: &CommitBase) -> (Scalar, Scalar) {
+//     let batch_dom = BatchEvaluationDomain::new(n);
+//     let lagr = lagrange_coefficients_at_zero(&batch_dom, players.as_slice());
 
-    let mut s = Scalar::zero();
-    let mut r = Scalar::zero();
+//     let mut s = Scalar::zero();
+//     let mut r = Scalar::zero();
 
-    // 转化为所需类型的格式
-    let mut shares: Vec<[Scalar; 2]> = share_f
-    .iter()
-    .zip(share_r)
-    .map(|(a, b)| [a.clone(), b.clone()])
-    .collect();
+//     // 转化为所需类型的格式
+//     let mut shares: Vec<[Scalar; 2]> = share_f
+//     .iter()
+//     .zip(share_r)
+//     .map(|(a, b)| [a.clone(), b.clone()])
+//     .collect();
 
-    let t = shares.len();
-    for i in 0..t {
-        let com = G1Projective::multi_exp(commit_base.bases.as_slice(), shares[i].as_slice().clone());
-        assert!(ct_coms[players[i]].eq(&com));
+//     let t = shares.len();
+//     for i in 0..t {
+//         let com = G1Projective::multi_exp(commit_base.bases.as_slice(), shares[i].as_slice().clone());
+//         assert!(ct_coms[players[i]].eq(&com));
 
-        s += lagr[i].mul(shares[i][0]);
-        r += lagr[i].mul(shares[i][1]);
-    }
+//         s += lagr[i].mul(shares[i][0]);
+//         r += lagr[i].mul(shares[i][1]);
+//     }
 
-    (s, r)
-}
+//     (s, r)
+// }
 
 // 最后一步重构
 // players 表示所需重构的prover的个数
@@ -46,6 +46,24 @@ pub fn reconstruct_y(ct_coms: &Vec<G1Projective>, y_k: &Vec<Scalar>, players: &V
     for i in 0..t {
         Y += lagr[i].mul(y_k[i]);
     }
-
+    
     Y
+
+}
+
+// 调用m次来恢复f_1(0),...,f_m(0)，n是Client的个数
+// players 表示所需重构的prover的个数，share_f和share_r对应其拥有的秘密份额
+// prover 重构出请求的第i个Client的commitment
+pub fn reconstruct_com(ct_com: &Vec<Scalar>, players: &Vec<usize>, n:usize) -> Scalar {
+    let batch_dom = BatchEvaluationDomain::new(n);
+    let lagr = lagrange_coefficients_at_zero(&batch_dom, players.as_slice());
+
+    let mut com_i = Scalar::zero();
+
+    let t = ct_com.len();
+    for i in 0..t {
+        com_i += lagr[i].mul(ct_com[i]);
+    }
+
+    com_i
 }
