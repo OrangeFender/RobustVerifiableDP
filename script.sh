85231f396e66b32765df4a1c1267912794d16ad3
OrangeFender <zyfdmap@126.com>
2025-01-07 14:38:56 +0800

    update script.sh

--- script.sh
+++ script.sh
@@ -3,12 +3,12 @@ rm Output.txt
 example_name="$1"
 
 
-m5_t2>> Output.txt
+echo "m5_t2" >> Output.txt
 cargo run --package robust_verifiable_dp --example $example_name --release --no-default-features --features m5_t2 >> Output.txt
-m5_t4>> Output.txt
+echo "m5_t4" >> Output.txt
 cargo run --package robust_verifiable_dp --example $example_name --release --no-default-features --features m5_t4 >> Output.txt
-m3_t1>> Output.txt
+echo "m3_t1" >> Output.txt
 cargo run --package robust_verifiable_dp --example $example_name --release --no-default-features --features m3_t1 >> Output.txt
-m3_t2>> Output.txt
+echo "m3_t2" >> Output.txt
 cargo run --package robust_verifiable_dp --example $example_name --release --no-default-features --features m3_t2 >> Output.txt
 
