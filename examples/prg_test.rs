03470ca85e3ccac83031568acb379d14cf368261
OrangeFender <zyfdmap@126.com>
2025-01-08 18:01:59 +0800

    update prg_test

--- examples/prg_test.rs
+++ examples/prg_test.rs
@@ -1,5 +1,5 @@
 extern crate robust_verifiable_dp as dp;
-use dp::prg::prg_mp;
+use dp::prg::{prg_mp, prg};
 use dp::constants;
 
 use std::time::Instant;
@@ -9,5 +9,12 @@ fn main(){
     let bitslen = constants::BITS_NUM*constants::SHARE_LEN;
     let result = prg_mp(&seed, bitslen);
     assert_eq!(result.len(), bitslen / 8);
-    println!("Time elapsed in prg is: {:?}", start.elapsed());
+    println!("Time elapsed in prg mp is: {:?}", start.elapsed());
+
+    let start = Instant::now();
+    let seed = [0u8; 16];
+    let bitslen = constants::BITS_NUM*constants::SHARE_LEN;
+    let result = prg(&seed, bitslen);
+    assert_eq!(result.len(), bitslen / 8);
+    println!("Time elapsed in prg mp is: {:?}", start.elapsed());
 }
