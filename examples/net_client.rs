99f1c14035ab188e884b4d57fd72eca9cba4b665
OrangeFender <zyfdmap@126.com>
2025-01-08 21:24:00 +0800

    add network

---
+++ examples/net_client.rs
@@ -0,0 +1,25 @@
+
+extern crate robust_verifiable_dp as dp;
+
+use std::net::{TcpStream, TcpListener};
+use std::time::Instant;
+use std::env;
+
+use std::io::prelude::*;
+
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <server_ip>", args[0]);
+        std::process::exit(1);
+    }
+    let server_ip = &args[1];
+    let start = Instant::now();
+    let mut stream = TcpStream::connect(server_ip).unwrap();
+    let mut buffer = [0; 32*256]; // Assuming the share is 32 bytes long
+    stream.read_exact(&mut buffer).unwrap();
+    println!("Received share");
+    println!("time elapsed is: {:?}", start.elapsed());
+}
---
+++ examples/net_prover.rs
@@ -0,0 +1,28 @@
+
+extern crate robust_verifiable_dp as dp;
+
+use std::net::{TcpListener, TcpStream};
+use std::io::Write;
+
+
+
+fn main() {
+
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    println!("Server listening on port 7878");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                let bytes = [7u8; 32*256];
+
+                stream.write_all(&bytes).unwrap();
+            }
+            Err(e) => {
+                eprintln!("Connection failed: {}", e);
+            }
+        }
+    }
+
+    
+}
